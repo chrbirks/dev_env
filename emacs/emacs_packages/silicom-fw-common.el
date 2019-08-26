376d38c1961c9a0da970a146b9b32a7b5d747f84
Christian Birk Sørensen <cbs@silicom.dk>
2019-08-26 08:58:21 +0200

    Updated silicom-fw-common.el

--- emacs/emacs_packages/silicom-fw-common.el
+++ emacs/emacs_packages/silicom-fw-common.el
@@ -54,41 +54,38 @@
 (setq vhdl-company-name silicom-company-name
       vhdl-date-format "%Y-%m-%d"
       vhdl-file-header
-      "-- *************************************************************************
--- *
--- * Copyright (c) 2008-2019, <company>
--- * All rights reserved.
--- *
--- * Redistribution and use in source and binary forms, with or without
--- * modification, are permitted provided that the following conditions are met:
--- *
--- * 1. Redistributions of source code must retain the above copyright notice,
--- * this list of conditions and the following disclaimer.
--- *
--- * 2. Redistributions in binary form must reproduce the above copyright
--- * notice, this list of conditions and the following disclaimer in the
--- * documentation and/or other materials provided with the distribution.
--- *
--- * 3. Neither the name of the Silicom nor the names of its
--- * contributors may be used to endorse or promote products derived from
--- * this software without specific prior written permission.
--- *
--- * 4. This software may only be redistributed and used in connection with a
--- *  Silicom network adapter product.
--- *
--- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\"
--- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
--- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
--- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
--- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
--- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
--- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
--- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
--- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
--- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
--- * POSSIBILITY OF SUCH DAMAGE.
--- *
--- ***************************************************************************
+      "-------------------------------------------------------------------------------
+-- Copyright (c) 2008-2019, <company>
+-- All rights reserved.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- 1. Redistributions of source code must retain the above copyright notice,
+-- this list of conditions and the following disclaimer.
+--
+-- 2. Redistributions in binary form must reproduce the above copyright
+-- notice, this list of conditions and the following disclaimer in the
+-- documentation and/or other materials provided with the distribution.
+--
+-- 3. Neither the name of the Silicom nor the names of its
+-- contributors may be used to endorse or promote products derived from
+-- this software without specific prior written permission.
+--
+-- 4. This software may only be redistributed and used in connection with a
+--  Silicom network adapter product.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
 -------------------------------------------------------------------------------
 -- Title      : <title string>
 -- Project    : <project string>
@@ -108,8 +105,9 @@
       vhdl-align-same-indent t
       vhdl-auto-align t
       vhdl-basic-offset 3
+      vhdl-beautify-options (quote (nil t t t t)) ; whitespace cleanup, single state per line, indentation, alignment, case fixing TODO: move back into silicom-fw-common package
       vhdl-electric-mode nil
-      vhdl-end-comment-column 80
+      vhdl-end-comment-column 120
       vhdl-indent-comment-like-next-code-line t
       vhdl-indent-syntax-based t
       vhdl-indent-tabs-mode nil
